0fa7f9f8a91d415fc4db37f035c1acd9bd9a9ea0
Nikita Naleikin <trubatvoia3@gmail.com>
2021-01-26 10:48:51 +0400

    Saves the app state in the browser's session storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,6 +5,24 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from '../reducers/rootReducer';
 import { State } from '../types/state';
 
-const store: Store<State> = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const saveState = (state: State) => {
+  const stateJSON = JSON.stringify(state);
+  window.sessionStorage.setItem('appState', stateJSON);
+};
+
+const loadState = () => {
+  const savedState = window.sessionStorage.getItem('appState');
+  return savedState ? (JSON.parse(savedState) as State) : undefined;
+};
+
+const savedState = loadState();
+
+const store: Store<State> = createStore(
+  rootReducer,
+  savedState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.subscribe(() => saveState(store.getState()));
 
 export { store };
